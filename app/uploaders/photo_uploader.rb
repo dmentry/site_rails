7d1c63d4c2a04f05b5ae3f8004537b31f90e9f86
dack99 <slipin2020@mail.ru>
2023-07-17 01:30:47 +0300

    Equal width and height of the photos at desktpo nd mobile view #2

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -36,7 +36,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_limit: [0, 500]
+    process resize_to_limit: [500, 500]
+    # process resize_to_fit: [500, 500]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
