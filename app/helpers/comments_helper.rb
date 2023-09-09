73629e5088deed091f66ed43b34aee111e1bcf1d
dack99 <slipin2020@mail.ru>
2023-09-10 01:36:07 +0300

    Comments in blogs show hierarchically

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,38 +1,19 @@
 module CommentsHelper
-def sort_comments(comments:, answers:)
-  comments = comments.to_a
-  answers = answers.to_a
-
-  comments_w_replies = []
-
-  sorted_replies = recursive_sort(answers)
-
-  sorted_replies_tmp = sorted_replies.map(&:clone)
-
-  comments.each do |comment|
-    comments_w_replies << comment
-
-    sorted_replies.each do |reply|
-      if comment.id == reply.opinion_id
-        comments_w_replies << reply
-
-        next
-      end
+  class CommentSort
+    def initialize
+      @out = []
     end
-  end
 
-  comments_w_replies + (sorted_replies_tmp - comments_w_replies)
+    def sort_comments(comments, otstup)
+      comments.each do |comment|
+        @out << [comment, otstup]
 
-  end
+        if comment.replies.present?
+          sort_comments(comment.replies, otstup + 1)
+        end
+      end
 
-  private
-
-  def recursive_sort(arr)
-    return arr if arr.length <= 1
-
-    pivot = arr.shift
-    left, right = arr.partition { |el| el.opinion_id < pivot.opinion_id || (el.opinion_id == pivot.opinion_id && el.id < pivot.id) }
-
-    recursive_sort(left) + [pivot] + recursive_sort(right)
+      @out
+    end
   end
 end
