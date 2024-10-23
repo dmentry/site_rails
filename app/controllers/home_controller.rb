4112c9b7272ee7af04b7851946f399265da7dbde
dack99 <slipin2020@mail.ru>
2024-10-24 01:23:11 +0300

    fix search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -44,7 +44,7 @@ class HomeController < ApplicationController
 
       articles = Article.ransack(article_body_ru_or_article_body_en_or_article_title_ru_or_article_title_ru_cont: q).result
       photos   = Photo.ransack(description_ru_or_description_en_cont: q).result
-      about    = About.ransack(main_text_ru_or_dmain_text_en_cont: q).result
+      about    = About.ransack(main_text_ru_or_main_text_en_cont: q).result
 
       @results = if params[:sorting] && params[:sorting] == 'up'
                   (articles + photos + about).sort_by(&:created_at)
